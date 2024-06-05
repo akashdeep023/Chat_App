e2edc102d59683bf1fe97de1825bf8c2c0dad5b9
akashdeep023 <ad3500476@gmail.com>
2024-06-05 18:31:06 +0530

    Update UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,58 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Boy_Img from "../assets/boy.png";
 import Girl_Img from "../assets/girl.png";
-import { FaEllipsisV, FaFolderOpen, FaPaperPlane } from "react-icons/fa";
+import {
+	FaArrowAltCircleDown,
+	FaEllipsisV,
+	FaFolderOpen,
+	FaPaperPlane,
+	FaPenAlt,
+} from "react-icons/fa";
+import { MdOutlineClose } from "react-icons/md";
 
 const Home = () => {
+	const chatBox = useRef();
+	const [scrollShow, setScrollShow] = useState(true);
+	const [chatMenuBtn, setChatMenuBtn] = useState(false);
+	const handleScrollDownChat = () => {
+		if (chatBox.current) {
+			chatBox.current.scrollTo({
+				top: chatBox.current.scrollHeight,
+				behavior: "smooth",
+			});
+		}
+	};
+	// Scroll btn hide
+	useEffect(() => {
+		const handleScroll = () => {
+			const currentScrollPos = chatBox.current.scrollTop;
+			if (
+				currentScrollPos + chatBox.current.clientHeight <
+				chatBox.current.scrollHeight - 100
+			) {
+				setScrollShow(true);
+			} else {
+				setScrollShow(false);
+			}
+		};
+		const chatBoxCurrent = chatBox.current;
+		chatBoxCurrent.addEventListener("scroll", handleScroll);
+		return () => {
+			chatBoxCurrent.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+	useEffect(() => {
+		handleScrollDownChat();
+	}, []);
 	return (
 		<div className="flex w-full border border-slate-800 rounded-sm shadow-md shadow-black">
 			<div className="w-0 sm:w-[40%] h-[80vh] relative">
-				<h1 className="p-6 w-full h-[7vh] font-semibold flex justify-between items-center sticky top-0 left-0 bg-slate-800 text-white border-r">
-					ChatsApp
-				</h1>
+				<div className="p-6 w-full h-[7vh] font-semibold flex justify-between items-center sticky top-0 left-0 bg-slate-800 text-white border-r">
+					<h1>ChatsApp</h1>
+					<FaPenAlt />
+				</div>
 				<div className="flex flex-col w-full px-4 gap-1 py-2 overflow-y-scroll overflow-hidden scroll-style h-[73vh]">
-					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 hover:bg-slate-400 cursor-pointer">
+					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 text-white hover:text-black transition-all hover:bg-slate-400 cursor-pointer">
 						<img
 							className="h-12 w-12 rounded-full"
 							src={Boy_Img}
@@ -21,7 +62,7 @@ const Home = () => {
 							Group Name Group Name Group Name
 						</span>
 					</div>
-					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 hover:bg-slate-400 cursor-pointer">
+					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 text-white hover:text-black transition-all hover:bg-slate-400 cursor-pointer">
 						<img
 							className="h-12 w-12 rounded-full"
 							src={Girl_Img}
@@ -29,7 +70,7 @@ const Home = () => {
 						/>
 						<span className="line-clamp-1">Group Name</span>
 					</div>
-					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 hover:bg-slate-400 cursor-pointer">
+					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 text-white hover:text-black transition-all hover:bg-slate-400 cursor-pointer">
 						<img
 							className="h-12 w-12 rounded-full"
 							src={Boy_Img}
@@ -39,7 +80,7 @@ const Home = () => {
 							Group Name Group Name Group Name
 						</span>
 					</div>
-					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 hover:bg-slate-400 cursor-pointer">
+					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 text-white hover:text-black transition-all hover:bg-slate-400 cursor-pointer">
 						<img
 							className="h-12 w-12 rounded-full"
 							src={Girl_Img}
@@ -47,7 +88,7 @@ const Home = () => {
 						/>
 						<span className="line-clamp-1">Group Name</span>
 					</div>
-					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 hover:bg-slate-400 cursor-pointer">
+					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 text-white hover:text-black transition-all hover:bg-slate-400 cursor-pointer">
 						<img
 							className="h-12 w-12 rounded-full"
 							src={Boy_Img}
@@ -57,7 +98,7 @@ const Home = () => {
 							Group Name Group Name Group Name
 						</span>
 					</div>
-					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 hover:bg-slate-400 cursor-pointer">
+					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 text-white hover:text-black transition-all hover:bg-slate-400 cursor-pointer">
 						<img
 							className="h-12 w-12 rounded-full"
 							src={Girl_Img}
@@ -65,7 +106,7 @@ const Home = () => {
 						/>
 						<span className="line-clamp-1">Group Name</span>
 					</div>
-					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 hover:bg-slate-400 cursor-pointer">
+					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 text-white hover:text-black transition-all hover:bg-slate-400 cursor-pointer">
 						<img
 							className="h-12 w-12 rounded-full"
 							src={Boy_Img}
@@ -75,7 +116,7 @@ const Home = () => {
 							Group Name Group Name Group Name
 						</span>
 					</div>
-					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 hover:bg-slate-400 cursor-pointer">
+					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 text-white hover:text-black transition-all hover:bg-slate-400 cursor-pointer">
 						<img
 							className="h-12 w-12 rounded-full"
 							src={Girl_Img}
@@ -83,7 +124,7 @@ const Home = () => {
 						/>
 						<span className="line-clamp-1">Group Name</span>
 					</div>
-					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 hover:bg-slate-400 cursor-pointer">
+					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 text-white hover:text-black transition-all hover:bg-slate-400 cursor-pointer">
 						<img
 							className="h-12 w-12 rounded-full"
 							src={Boy_Img}
@@ -93,7 +134,7 @@ const Home = () => {
 							Group Name Group Name Group Name
 						</span>
 					</div>
-					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 hover:bg-slate-400 cursor-pointer">
+					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 text-white hover:text-black transition-all hover:bg-slate-400 cursor-pointer">
 						<img
 							className="h-12 w-12 rounded-full"
 							src={Girl_Img}
@@ -101,7 +142,7 @@ const Home = () => {
 						/>
 						<span className="line-clamp-1">Group Name</span>
 					</div>
-					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 hover:bg-slate-400 cursor-pointer">
+					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 text-white hover:text-black transition-all hover:bg-slate-400 cursor-pointer">
 						<img
 							className="h-12 w-12 rounded-full"
 							src={Boy_Img}
@@ -111,7 +152,7 @@ const Home = () => {
 							Group Name Group Name Group Name
 						</span>
 					</div>
-					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 hover:bg-slate-400 cursor-pointer">
+					<div className="w-full h-16 border rounded-lg flex justify-start items-center p-2 font-semibold gap-2 text-white hover:text-black transition-all hover:bg-slate-400 cursor-pointer">
 						<img
 							className="h-12 w-12 rounded-full"
 							src={Girl_Img}
@@ -124,9 +165,36 @@ const Home = () => {
 			<div className="sm:w-[60%] w-full h-[80vh] relative">
 				<div className="p-6 w-full h-[7vh] font-semibold flex justify-between items-center sticky top-0 left-0 bg-slate-800 text-white">
 					<h1>Group Name</h1>
-					<FaEllipsisV className="cursor-pointer" />
+					<FaEllipsisV
+						className="cursor-pointer"
+						onClick={() => setChatMenuBtn(true)}
+					/>
 				</div>
-				<div className="flex flex-col w-full px-4 gap-1 py-2 overflow-y-scroll overflow-hidden scroll-style h-[66vh]">
+				{scrollShow && (
+					<div
+						className="absolute bottom-16 right-5 cursor-pointer opacity-80"
+						onClick={handleScrollDownChat}
+					>
+						<FaArrowAltCircleDown size={30} />
+					</div>
+				)}
+				{chatMenuBtn && (
+					<div className="border rounded-md absolute top-2 right-2 bg-slate-800 text-white flex items-center justify-center gap-4 w-48 h-48 ">
+						<MdOutlineClose
+							size={22}
+							className="absolute top-2 right-3 cursor-pointer"
+							onClick={() => setChatMenuBtn(false)}
+						/>
+						<div className="flex flex-col items-start gap-4 justify-center">
+							<span>Setting</span>
+							<span>Clear Chat</span>
+						</div>
+					</div>
+				)}
+				<div
+					className="flex flex-col w-full px-4 gap-1 py-2 overflow-y-scroll overflow-hidden scroll-style h-[66vh]"
+					ref={chatBox}
+				>
 					<span className="self-end border rounded-lg py-1 px-2 bg-green-700 text-white">
 						Hi
 					</span>
@@ -200,7 +268,10 @@ const Home = () => {
 					/>
 					<span>
 						<button className="outline-none p-2 border-l">
-							<FaPaperPlane size={18} />
+							<FaPaperPlane
+								size={18}
+								className="active:scale-75 hover:text-green-400"
+							/>
 						</button>
 					</span>
 				</div>
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,7 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
-	return <div>SignIn</div>;
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [inputfield, setInputField] = useState("");
+	const [load, setLoad] = useState("");
+	const logInUser = () => {
+		// SignIn ---
+	};
+	const handleLogin = () => {
+		if (username && password) {
+			setInputField("");
+			setLoad("Loading...");
+			logInUser();
+		} else {
+			setInputField("All input fields are not valid");
+		}
+	};
+	return (
+		<div className="flex flex-col items-center my-6 text-slate-300 min-h-[80vh]">
+			<div className="p-3 w-[80%] sm:w-[60%] md:w-[50%] lg:w-[40%] min-w-72 max-w-[1000px] border border-slate-400 bg-slate-800 rounded-lg h-fit  mt-5 transition-all">
+				<h2 className="text-2xl underline underline-offset-8 font-semibold text-slate-100 w-full text-center mb-4">
+					SignIn ChatApp
+				</h2>
+				<form className="w-full flex justify-between flex-col">
+					<h3 className="text-xl font-semibold p-1">
+						Enter Username
+					</h3>
+					<input
+						className="w-full border border-slate-700 my-3 py-4 px-8 rounded-full flex justify-between bg-white text-black "
+						type="text"
+						placeholder="Enter Username"
+						name="username"
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+					<h3 className="text-xl font-semibold p-1">
+						Enter Password
+					</h3>
+					<input
+						className="w-full border border-slate-700 my-3 py-4 px-8 rounded-full flex justify-between bg-white text-black "
+						type="password"
+						placeholder="Enter Password"
+						name="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<div className="text-red-600 font-semibold self-center">
+						{inputfield}
+					</div>
+					<button
+						onClick={(e) => {
+							handleLogin();
+							e.preventDefault();
+						}}
+						className="w-full font-semibold hover:bg-black rounded-full px-5 py-4 mt-5 text-lg border border-slate-400  text-slate-400 hover:text-white bg-slate-700 transition-all"
+					>
+						{load == "" ? "SignIn" : load}
+					</button>
+					<div className="w-full flex items-center mt-3">
+						<div className="w-full h-[1px] bg-slate-600"></div>
+						<Link to={"#"}>
+							<div className="p-3 font-semibold text-md hover:text-white whitespace-nowrap">
+								Forgot Password
+							</div>
+						</Link>
+						<div className="w-full h-[1px] bg-slate-600"></div>
+					</div>
+					<div className="w-full flex items-center my-3">
+						<div className="w-full h-[1px] bg-slate-600"></div>
+						<Link to="/signup">
+							<div className="p-3 font-semibold text-md hover:text-white">
+								SignUp
+							</div>
+						</Link>
+						<div className="w-full h-[1px] bg-slate-600"></div>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default SignIn;
