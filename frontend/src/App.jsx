ec384a296743bbd3d62b57a6e68d2360817fbeba
akashdeep023 <ad3500476@gmail.com>
2024-06-05 18:33:08 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,28 @@
-import React from "react";
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+	createBrowserRouter,
+	Outlet,
+	RouterProvider,
+	useLocation,
+} from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Error from "./pages/Error";
+import handleScrollTop from "./utils/handleScrollTop";
 
 const Applayout = () => {
+	// Scroll to top of page --------------------------------
+	const { pathname } = useLocation();
+	useEffect(() => {
+		handleScrollTop();
+	}, [pathname]);
 	return (
 		<div>
 			<Header />
-			<div className="min-h-[90vh] p-4">
+			<div className="min-h-[85vh] p-4 bg-gradient-to-tr to-black via-blue-900 from-black">
 				<Outlet />
 			</div>
 			<Footer />
