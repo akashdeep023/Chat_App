afc1cc4fb453feb0270fe9339f7a16f981c00ca2
akashdeep023 <ad3500476@gmail.com>
2024-06-07 04:12:48 +0530

    Create Route && Error Handling

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,34 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+const connectDb = require("./config/db");
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
 	res.json({ message: "Welcome to Chat Application!" });
 });
 
-app.listen(9000, () => console.log("Server listening on 9000"));
+const authRouter = require("./routes/auth");
+app.use("/api/auth", authRouter);
+const userRouter = require("./routes/user");
+app.use("/api/user", userRouter);
+
+app.all("*", (req, res) => {
+	res.json({ error: "Invalid Route" });
+});
+
+app.use((err, req, res, next) => {
+	res.status(500).json({ message: "Something Went Worng!" });
+});
+
+app.listen(PORT, () => {
+	connectDb();
+	console.log(`Server listening on ${PORT}`);
+});
