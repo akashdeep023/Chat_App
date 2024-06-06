d092c1cb379957ecda1cac0ffb136e76e9ac924b
akashdeep023 <ad3500476@gmail.com>
2024-06-07 04:59:07 +0530

    Update Cors Error

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,15 @@ const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
-const mongoose = require("mongoose");
 const connectDb = require("./config/db");
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+// Use the CORS middleware
+app.use(
+	cors({
+		origin: "https://chat-application-jack.vercel.app",
+	})
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
