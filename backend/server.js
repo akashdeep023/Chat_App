bd4bdded6b046124105bf01e1b58ec1206386add
akashdeep023 <ad3500476@gmail.com>
2024-06-07 17:34:42 +0530

    Update db

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,21 @@ const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
-const connectDb = require("./config/db");
+// const connectDb = require("./config/db");
 const PORT = process.env.PORT || 3000;
+
+const connectDb = async () => {
+	try {
+		if (!process.env.ATLASDB_URL) {
+			throw new Error("ATLASDB_URL environment variable is not defined");
+		}
+		await mongoose.connect(process.env.ATLASDB_URL);
+		console.log("Database Connection established");
+	} catch (err) {
+		console.error("Database connection error:", err);
+	}
+};
+connectDb();
 
 // Use the CORS middleware
 app.use(cors());
@@ -29,6 +42,6 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, async () => {
-	await connectDb();
+	// await connectDb();
 	console.log(`Server listening on ${PORT}`);
 });
