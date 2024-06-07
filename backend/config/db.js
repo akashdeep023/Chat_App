ae978cfd0c029c84a7d0e91f4e24149ec341ea75
akashdeep023 <ad3500476@gmail.com>
2024-06-07 21:30:17 +0530

    Fix bug

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
 	try {
-		if (!process.env.ATLASDB_URL) {
-			throw new Error("ATLASDB_URL environment variable is not defined");
+		if (!process.env.MONGODB_URI) {
+			throw new Error("MONGODB_URI environment variable is not defined");
 		}
-		await mongoose.connect(process.env.ATLASDB_URL);
+		await mongoose.connect(process.env.MONGODB_URI);
 		console.log("Database Connection established");
 	} catch (err) {
 		console.error("Database connection error:", err);
