e7200608a28dcccba7c3020cf8c73a52f5d3f30d
akashdeep023 <ad3500476@gmail.com>
2024-06-11 03:44:08 +0530

    Update file

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const userControllers = require("../controllers/user");
 const wrapAsync = require("../middlewares/wrapAsync");
+const { authorization } = require("../middlewares/authorization");
 
-router.get("/profile", wrapAsync(userControllers.getAuthUser));
-router.get("/users", wrapAsync(userControllers.getAllUsers));
+router.get("/profile", authorization, wrapAsync(userControllers.getAuthUser));
+router.get("/users", authorization, wrapAsync(userControllers.getAllUsers));
 
 module.exports = router;
