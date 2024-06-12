eed89c9c20fc9d05d830db97caebaa4a73446ce6
akashdeep023 <ad3500476@gmail.com>
2024-06-13 02:26:13 +0530

    Create component chat not selected

---
+++ frontend/src/components/ChatNotSelected.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChatNotSelected = () => {
+	return (
+		<div className="sm:w-[60%] w-full h-[80vh] bg-black/40 relative text-white flex justify-center items-center font-semibold">
+			<h1>Select a chat to start messaging</h1>
+		</div>
+	);
+};
+
+export default ChatNotSelected;
