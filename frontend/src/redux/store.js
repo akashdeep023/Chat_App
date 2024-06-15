216ca98e490099075580265e9bade9b1f66eb2aa
akashdeep023 <ad3500476@gmail.com>
2024-06-16 00:52:28 +0530

    Update redux and delete slice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./auth/authSlice";
-import usersSlice from "./auth/usersSlice";
 import conditionSlice from "./auth/conditionSlice";
 import myChatSlice from "./auth/myChatSlice";
 
 const store = configureStore({
 	reducer: {
 		auth: authSlice,
-		users: usersSlice,
 		condition: conditionSlice,
 		myChat: myChatSlice,
 	},
