1a86d60a6d22830d81ae0dc546c581c62e64b58b
akashdeep023 <ad3500476@gmail.com>
2024-07-02 00:31:37 +0530

    Add newMessage

--- frontend/src/redux/slices/conditionSlice.js
+++ frontend/src/redux/slices/conditionSlice.js
@@ -1,59 +1,64 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const conditionSlice = createSlice({
-	name: "condition",
-	initialState: {
-		isLoading: false,
-		isChatLoading: false,
-		isMessageLoading: false,
-		isSendLoading: false,
-		isProfileDetail: false,
-		isHeaderMenu: false,
-		isChatDetailsBox: false,
-		isUserSearchBox: false,
-		isGroupChatBox: false,
-	},
-	reducers: {
-		setLoading: (state, action) => {
-			state.isLoading = action.payload;
-		},
-		setChatLoading: (state, action) => {
-			state.isChatLoading = action.payload;
-		},
-		setMessageLoading: (state, action) => {
-			state.isMessageLoading = action.payload;
-		},
-		setSendLoading: (state, action) => {
-			state.isSendLoading = action.payload;
-		},
-		setProfileDetail: (state, action) => {
-			state.isProfileDetail = !state.isProfileDetail;
-		},
-		setHeaderMenu: (state, action) => {
-			state.isHeaderMenu = action.payload;
-			state.isChatDetailsBox = false;
-		},
-		setChatDetailsBox: (state, action) => {
-			state.isHeaderMenu = false;
-			state.isChatDetailsBox = action.payload;
-		},
-		setUserSearchBox: (state, action) => {
-			state.isUserSearchBox = !state.isUserSearchBox;
-		},
-		setGroupChatBox: (state, action) => {
-			state.isGroupChatBox = !state.isGroupChatBox;
-		},
-	},
+    name: "condition",
+    initialState: {
+        isLoading: false,
+        isChatLoading: false,
+        isMessageLoading: false,
+        isSendLoading: false,
+        isProfileDetail: false,
+        isHeaderMenu: false,
+        isChatDetailsBox: false,
+        isUserSearchBox: false,
+        isGroupChatBox: false,
+        isGroupChatId: "",
+    },
+    reducers: {
+        setLoading: (state, action) => {
+            state.isLoading = action.payload;
+        },
+        setChatLoading: (state, action) => {
+            state.isChatLoading = action.payload;
+        },
+        setMessageLoading: (state, action) => {
+            state.isMessageLoading = action.payload;
+        },
+        setSendLoading: (state, action) => {
+            state.isSendLoading = action.payload;
+        },
+        setProfileDetail: (state, action) => {
+            state.isProfileDetail = !state.isProfileDetail;
+        },
+        setHeaderMenu: (state, action) => {
+            state.isHeaderMenu = action.payload;
+            state.isChatDetailsBox = false;
+        },
+        setChatDetailsBox: (state, action) => {
+            state.isHeaderMenu = false;
+            state.isChatDetailsBox = action.payload;
+        },
+        setUserSearchBox: (state, action) => {
+            state.isUserSearchBox = !state.isUserSearchBox;
+        },
+        setGroupChatBox: (state, action) => {
+            state.isGroupChatBox = !state.isGroupChatBox;
+        },
+        setGroupChatId: (state, action) => {
+            state.isGroupChatId = action.payload;
+        },
+    },
 });
 export const {
-	setLoading,
-	setChatLoading,
-	setMessageLoading,
-	setSendLoading,
-	setProfileDetail,
-	setHeaderMenu,
-	setChatDetailsBox,
-	setUserSearchBox,
-	setGroupChatBox,
+    setLoading,
+    setChatLoading,
+    setMessageLoading,
+    setSendLoading,
+    setProfileDetail,
+    setHeaderMenu,
+    setChatDetailsBox,
+    setUserSearchBox,
+    setGroupChatBox,
+    setGroupChatId,
 } = conditionSlice.actions;
 export default conditionSlice.reducer;
--- frontend/src/redux/slices/messageSlice.js
+++ frontend/src/redux/slices/messageSlice.js
@@ -1,20 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const messageSlice = createSlice({
-	name: "message",
-	initialState: {
-		message: [],
-		newMessageId: "",
-	},
-	reducers: {
-		addAllMessages: (state, action) => {
-			state.message = action.payload;
-		},
-		addNewMessageId: (state, action) => {
-			state.newMessageId = action.payload;
-		},
-	},
+    name: "message",
+    initialState: {
+        message: [],
+        newMessageId: "",
+    },
+    reducers: {
+        addAllMessages: (state, action) => {
+            state.message = action.payload;
+        },
+        addNewMessage: (state, action) => {
+            state.message = [...state.message, action.payload];
+        },
+        addNewMessageId: (state, action) => {
+            state.newMessageId = action.payload;
+        },
+    },
 });
 
-export const { addAllMessages, addNewMessageId } = messageSlice.actions;
+export const { addAllMessages, addNewMessage, addNewMessageId } =
+    messageSlice.actions;
 export default messageSlice.reducer;
