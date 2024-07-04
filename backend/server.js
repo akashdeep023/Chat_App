0ad6a164f85790ca20140429e1294efce14395b1
akashdeep023 <ad3500476@gmail.com>
2024-07-04 23:44:50 +0530

    Add Typing Functionality

--- backend/server.js
+++ backend/server.js
@@ -68,7 +68,11 @@ io.on("connection", (socket) => {
         socket.join(room);
         console.log("User joined Room: " + room);
     });
-    //
+
+    socket.on("typing", (room) => socket.in(room).emit("typing"));
+
+    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
     socket.on("new message", (newMessageRecieved) => {
         let chat = newMessageRecieved.chat;
 
