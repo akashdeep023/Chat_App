e40af1aa35d46e5d93a582ea62c2cfe58881a5fb
akashdeep023 <ad3500476@gmail.com>
2024-07-05 00:18:00 +0530

    Loading Effect

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,182 +6,188 @@ import { addAuth } from "../redux/slices/authSlice";
 import handleScrollTop from "../utils/handleScrollTop";
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 import {
-	setHeaderMenu,
-	setProfileDetail,
+    setHeaderMenu,
+    setLoading,
+    setProfileDetail,
 } from "../redux/slices/conditionSlice";
 import { IoLogOutOutline } from "react-icons/io5";
 import { PiUserCircleLight } from "react-icons/pi";
 
 const Header = () => {
-	const user = useSelector((store) => store.auth);
-	const isHeaderMenu = useSelector((store) => store?.condition?.isHeaderMenu);
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
-	const token = localStorage.getItem("token");
-	const getAuthUser = (token) => {
-		fetch(`${import.meta.env.VITE_BACKEND_URL}/api/user/profile`, {
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: `Bearer ${token}`,
-			},
-		})
-			.then((res) => res.json())
-			.then((json) => {
-				dispatch(addAuth(json.data));
-			})
-			.catch((err) => console.log(err));
-	};
-	useEffect(() => {
-		if (token) {
-			getAuthUser(token);
-			navigate("/");
-		} else {
-			navigate("/signin");
-		}
-		dispatch(setHeaderMenu(false));
-	}, [token]);
+    const user = useSelector((store) => store.auth);
+    const isHeaderMenu = useSelector((store) => store?.condition?.isHeaderMenu);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const token = localStorage.getItem("token");
+    const getAuthUser = (token) => {
+        dispatch(setLoading(true));
+        fetch(`${import.meta.env.VITE_BACKEND_URL}/api/user/profile`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then((res) => res.json())
+            .then((json) => {
+                dispatch(addAuth(json.data));
+                dispatch(setLoading(false));
+            })
+            .catch((err) => {
+                console.log(err);
+                dispatch(setLoading(false));
+            });
+    };
+    useEffect(() => {
+        if (token) {
+            getAuthUser(token);
+            navigate("/");
+        } else {
+            navigate("/signin");
+        }
+        dispatch(setHeaderMenu(false));
+    }, [token]);
 
-	// Scroll to top of page && Redirect Auth change --------------------------------
-	const { pathname } = useLocation();
-	useEffect(() => {
-		if (user) {
-			navigate("/");
-		} else if (pathname !== "/signin" && pathname !== "/signup") {
-			navigate("/signin");
-		}
-		handleScrollTop();
-	}, [pathname, user]);
+    // Scroll to top of page && Redirect Auth change --------------------------------
+    const { pathname } = useLocation();
+    useEffect(() => {
+        if (user) {
+            navigate("/");
+        } else if (pathname !== "/signin" && pathname !== "/signup") {
+            navigate("/signin");
+        }
+        handleScrollTop();
+    }, [pathname, user]);
 
-	const handleLogout = () => {
-		localStorage.removeItem("token");
-		window.location.reload();
-		navigate("/signin");
-	};
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        window.location.reload();
+        navigate("/signin");
+    };
 
-	useEffect(() => {
-		var prevScrollPos = window.pageYOffset;
-		const handleScroll = () => {
-			var currentScrollPos = window.pageYOffset;
-			if (prevScrollPos < currentScrollPos && currentScrollPos > 80) {
-				document.getElementById("header").classList.add("hiddenbox");
-			} else {
-				document.getElementById("header").classList.remove("hiddenbox");
-			}
-			prevScrollPos = currentScrollPos;
-		};
-		window.addEventListener("scroll", handleScroll);
-		return () => {
-			window.removeEventListener("scroll", handleScroll);
-		};
-	}, []);
+    useEffect(() => {
+        var prevScrollPos = window.pageYOffset;
+        const handleScroll = () => {
+            var currentScrollPos = window.pageYOffset;
+            if (prevScrollPos < currentScrollPos && currentScrollPos > 80) {
+                document.getElementById("header").classList.add("hiddenbox");
+            } else {
+                document.getElementById("header").classList.remove("hiddenbox");
+            }
+            prevScrollPos = currentScrollPos;
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
-	const headerMenuBox = useRef(null);
-	const headerUserBox = useRef(null);
-	// headerMenuBox outside click handler
-	const handleClickOutside = (event) => {
-		if (
-			headerMenuBox.current &&
-			!headerUserBox?.current?.contains(event.target) &&
-			!headerMenuBox.current.contains(event.target)
-		) {
-			dispatch(setHeaderMenu(false));
-		}
-	};
+    const headerMenuBox = useRef(null);
+    const headerUserBox = useRef(null);
+    // headerMenuBox outside click handler
+    const handleClickOutside = (event) => {
+        if (
+            headerMenuBox.current &&
+            !headerUserBox?.current?.contains(event.target) &&
+            !headerMenuBox.current.contains(event.target)
+        ) {
+            dispatch(setHeaderMenu(false));
+        }
+    };
 
-	// add && remove events according to isHeaderMenu
-	useEffect(() => {
-		if (isHeaderMenu) {
-			document.addEventListener("mousedown", handleClickOutside);
-		} else {
-			document.removeEventListener("mousedown", handleClickOutside);
-		}
-		return () => {
-			document.removeEventListener("mousedown", handleClickOutside);
-		};
-	}, [isHeaderMenu]);
-	return (
-		<div
-			id="header"
-			className="w-full h-16 fixed top-0 z-50 md:h-20 shadow-gray-950 shadow-inner flex justify-between items-center p-4 font-semibold bg-slate-800 text-white"
-		>
-			<div className="flex items-center justify-start gap-2">
-				<Link to={"/"}>
-					<img
-						src={Logo}
-						alt="ChatApp"
-						className="h-12 w-12 rounded-tr-full rounded-tl-full rounded-br-full"
-					/>
-				</Link>
-				<Link to={"/"}>
-					<span>ChatApp</span>
-				</Link>
-			</div>
+    // add && remove events according to isHeaderMenu
+    useEffect(() => {
+        if (isHeaderMenu) {
+            document.addEventListener("mousedown", handleClickOutside);
+        } else {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [isHeaderMenu]);
+    return (
+        <div
+            id="header"
+            className="w-full h-16 fixed top-0 z-50 md:h-20 shadow-gray-950 shadow-inner flex justify-between items-center p-4 font-semibold bg-slate-800 text-white"
+        >
+            <div className="flex items-center justify-start gap-2">
+                <Link to={"/"}>
+                    <img
+                        src={Logo}
+                        alt="ChatApp"
+                        className="h-12 w-12 rounded-tr-full rounded-tl-full rounded-br-full"
+                    />
+                </Link>
+                <Link to={"/"}>
+                    <span>ChatApp</span>
+                </Link>
+            </div>
 
-			{user ? (
-				<div className="flex flex-nowrap items-center">
-					<span className="whitespace-nowrap ml-2">
-						Hi, {user.firstName}
-					</span>
-					<div
-						ref={headerUserBox}
-						onClick={(e) => {
-							e.preventDefault();
-							dispatch(setHeaderMenu(!isHeaderMenu));
-						}}
-						className="flex flex-nowrap transition-all items-center ml-3  border border-slate-400 rounded-full bg-gradient-to-tr to-slate-800 text-black via-white  from-slate-800 hover:bg-gradient-to-br shadow-sm  cursor-pointer"
-					>
-						<img
-							src={user.image}
-							alt="gg"
-							className="w-10 h-10 rounded-full"
-						/>
-						<span className="m-2">
-							{isHeaderMenu ? (
-								<MdKeyboardArrowDown fontSize={20} />
-							) : (
-								<MdKeyboardArrowUp fontSize={20} />
-							)}
-						</span>
-					</div>
-					{isHeaderMenu && (
-						<div
-							ref={headerMenuBox}
-							className="border border-slate-500 text-white w-40 h-24 py-2 flex flex-col justify-center rounded-md items-center gap-1 absolute top-16 right-4 z-40 bg-slate-700"
-						>
-							<div
-								onClick={() => {
-									dispatch(setHeaderMenu(false));
-									dispatch(setProfileDetail());
-								}}
-								className="flex flex-nowrap items-center w-full h-fit cursor-pointer justify-center hover:bg-slate-400 hover:text-black p-1"
-							>
-								<div className="flex items-center justify-between w-2/4">
-									<PiUserCircleLight fontSize={23} />
-									<span>Profile</span>
-								</div>
-							</div>
-							<div
-								className="flex flex-nowrap items-center w-full h-fit cursor-pointer justify-center hover:bg-slate-400 hover:text-black p-1"
-								onClick={handleLogout}
-							>
-								<div className="flex items-center justify-between w-2/4">
-									<IoLogOutOutline fontSize={21} />
-									<span>Logout</span>
-								</div>
-							</div>
-						</div>
-					)}
-				</div>
-			) : (
-				<Link to={"/signin"}>
-					<button className="py-2 px-4 border border-slate-400 rounded-full bg-gradient-to-tr to-slate-800 text-black via-white  from-slate-800 hover:bg-gradient-to-br shadow-sm hover:shadow-white">
-						SingIn
-					</button>
-				</Link>
-			)}
-		</div>
-	);
+            {user ? (
+                <div className="flex flex-nowrap items-center">
+                    <span className="whitespace-nowrap ml-2">
+                        Hi, {user.firstName}
+                    </span>
+                    <div
+                        ref={headerUserBox}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            dispatch(setHeaderMenu(!isHeaderMenu));
+                        }}
+                        className="flex flex-nowrap transition-all items-center ml-3  border border-slate-400 rounded-full bg-gradient-to-tr to-slate-800 text-black via-white  from-slate-800 hover:bg-gradient-to-br shadow-sm  cursor-pointer"
+                    >
+                        <img
+                            src={user.image}
+                            alt="gg"
+                            className="w-10 h-10 rounded-full"
+                        />
+                        <span className="m-2">
+                            {isHeaderMenu ? (
+                                <MdKeyboardArrowDown fontSize={20} />
+                            ) : (
+                                <MdKeyboardArrowUp fontSize={20} />
+                            )}
+                        </span>
+                    </div>
+                    {isHeaderMenu && (
+                        <div
+                            ref={headerMenuBox}
+                            className="border border-slate-500 text-white w-40 h-24 py-2 flex flex-col justify-center rounded-md items-center gap-1 absolute top-16 right-4 z-40 bg-slate-700"
+                        >
+                            <div
+                                onClick={() => {
+                                    dispatch(setHeaderMenu(false));
+                                    dispatch(setProfileDetail());
+                                }}
+                                className="flex flex-nowrap items-center w-full h-fit cursor-pointer justify-center hover:bg-slate-400 hover:text-black p-1"
+                            >
+                                <div className="flex items-center justify-between w-2/4">
+                                    <PiUserCircleLight fontSize={23} />
+                                    <span>Profile</span>
+                                </div>
+                            </div>
+                            <div
+                                className="flex flex-nowrap items-center w-full h-fit cursor-pointer justify-center hover:bg-slate-400 hover:text-black p-1"
+                                onClick={handleLogout}
+                            >
+                                <div className="flex items-center justify-between w-2/4">
+                                    <IoLogOutOutline fontSize={21} />
+                                    <span>Logout</span>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            ) : (
+                <Link to={"/signin"}>
+                    <button className="py-2 px-4 border border-slate-400 rounded-full bg-gradient-to-tr to-slate-800 text-black via-white  from-slate-800 hover:bg-gradient-to-br shadow-sm hover:shadow-white">
+                        SingIn
+                    </button>
+                </Link>
+            )}
+        </div>
+    );
 };
 
 export default Header;
