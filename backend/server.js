57843d1f31f3945e4f74557c91479e541605e69a
akashdeep023 <ad3500476@gmail.com>
2024-07-05 00:10:09 +0530

    Remove White space

--- backend/server.js
+++ backend/server.js
@@ -68,14 +68,11 @@ io.on("connection", (socket) => {
         socket.join(room);
         console.log("User joined Room: " + room);
     });
-
     socket.on("typing", (room) => socket.in(room).emit("typing"));
-
     socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
     socket.on("new message", (newMessageRecieved) => {
         let chat = newMessageRecieved.chat;
-
         chat.users.forEach((user) => {
             if (user._id === newMessageRecieved.sender._id) return;
             console.log("message rec : " + user._id);
