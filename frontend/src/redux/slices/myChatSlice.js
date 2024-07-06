962a5ce84353d088943d7af4859e9f23ee6ea91f
akashdeep023 <ad3500476@gmail.com>
2024-07-07 02:53:51 +0530

    Initialize notification && create action

--- frontend/src/redux/slices/myChatSlice.js
+++ frontend/src/redux/slices/myChatSlice.js
@@ -1,20 +1,40 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const myChatSlice = createSlice({
-	name: "myChat",
-	initialState: {
-		chat: [],
-		selectedChat: null,
-	},
-	reducers: {
-		addMyChat: (state, action) => {
-			state.chat = action.payload;
-		},
-		addSelectedChat: (state, action) => {
-			state.selectedChat = action.payload;
-		},
-	},
+    name: "myChat",
+    initialState: {
+        chat: [],
+        selectedChat: null,
+        newMessageRecieved: [],
+    },
+    reducers: {
+        addMyChat: (state, action) => {
+            state.chat = action.payload;
+        },
+        addSelectedChat: (state, action) => {
+            state.selectedChat = action.payload;
+        },
+        addNewMessageRecieved: (state, action) => {
+            if (!state.newMessageRecieved.includes(action.payload)) {
+                state.newMessageRecieved = [
+                    action.payload,
+                    ...state.newMessageRecieved,
+                ];
+            }
+        },
+        removeNewMessageRecieved: (state, action) => {
+            const newMessageRecieved = state.newMessageRecieved.filter(
+                (message) => message.chat._id !== action.payload.chat._id
+            );
+            state.newMessageRecieved = newMessageRecieved;
+        },
+    },
 });
 
-export const { addMyChat, addSelectedChat } = myChatSlice.actions;
+export const {
+    addMyChat,
+    addSelectedChat,
+    addNewMessageRecieved,
+    removeNewMessageRecieved,
+} = myChatSlice.actions;
 export default myChatSlice.reducer;
