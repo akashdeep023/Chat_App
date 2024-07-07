5a719222a58df79379fcdbf7604c0b2544880448
akashdeep023 <ad3500476@gmail.com>
2024-07-08 00:27:57 +0530

    Update cors error

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 const ENDPOINT = import.meta.env.VITE_BACKEND_URL;
 const socket = io(ENDPOINT, {
-    transports: ["websocket"],
-    withCredentials: true,
+	transports: ["websocket", "polling"],
+	withCredentials: true,
 });
 
 export default socket;
