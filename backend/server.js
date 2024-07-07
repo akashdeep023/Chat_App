3c0d407e1b7d0ee9f79d6e619b909a36e447a765
akashdeep023 <ad3500476@gmail.com>
2024-07-08 02:09:38 +0530

    production socket

--- backend/server.js
+++ backend/server.js
@@ -18,16 +18,16 @@ const messageRouter = require("./routes/message");
 
 // Connect to Database
 main()
-    .then(() => console.log("Database Connection established"))
-    .catch((err) => console.log(err));
+	.then(() => console.log("Database Connection established"))
+	.catch((err) => console.log(err));
 
 async function main() {
-    await mongoose.connect(process.env.MONGODB_URI);
+	await mongoose.connect(process.env.MONGODB_URI);
 }
 
 // Root route
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to Chat Application!" });
+	res.json({ message: "Welcome to Chat Application!" });
 });
 
 // All routes
@@ -38,85 +38,90 @@ app.use("/api/message", messageRouter);
 
 // Invaild routes
 app.all("*", (req, res) => {
-    res.json({ error: "Invaild Route" });
+	res.json({ error: "Invaild Route" });
 });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-    const errorMessage = err.message || "Something Went Wrong!";
-    res.status(500).json({ message: errorMessage });
+	const errorMessage = err.message || "Something Went Wrong!";
+	res.status(500).json({ message: errorMessage });
 });
 
 // Start the server
 const server = app.listen(PORT, async () => {
-    console.log(`Server listening on ${PORT}`);
+	console.log(`Server listening on ${PORT}`);
 });
 
 // Socket.IO setup
 const { Server } = require("socket.io");
 const io = new Server(server, {
-    pingTimeout: 60000,
-    cors: {
-        origin: process.env.FRONTEND_URL,
-        credentials: true,
-    },
+	pingTimeout: 60000,
+	path: "/socket",
+	wssEngine: ["ws", "wss"],
+	transports: ["websocket", "polling"],
+	allowEIO3: true,
+	cors: {
+		origin: "*",
+		// origin: process.env.FRONTEND_URL,
+		// credentials: true,
+	},
 });
 
 io.on("connection", (socket) => {
-    console.log("Connected to socket.io:", socket.id);
+	console.log("Connected to socket.io:", socket.id);
 
-    const setupHandler = (userId) => {
-        if (!socket.hasJoined) {
-            socket.join(userId);
-            socket.hasJoined = true;
-            console.log("User joined:", userId);
-            socket.emit("connected");
-        }
-    };
+	const setupHandler = (userId) => {
+		if (!socket.hasJoined) {
+			socket.join(userId);
+			socket.hasJoined = true;
+			console.log("User joined:", userId);
+			socket.emit("connected");
+		}
+	};
 
-    const joinChatHandler = (room) => {
-        if (socket.currentRoom) {
-            if (socket.currentRoom === room) {
-                console.log(`User already in Room: ${room}`);
-                return;
-            }
-            socket.leave(socket.currentRoom);
-            console.log(`User left Room: ${socket.currentRoom}`);
-        }
-        socket.join(room);
-        socket.currentRoom = room;
-        console.log("User joined Room:", room);
-    };
+	const joinChatHandler = (room) => {
+		if (socket.currentRoom) {
+			if (socket.currentRoom === room) {
+				console.log(`User already in Room: ${room}`);
+				return;
+			}
+			socket.leave(socket.currentRoom);
+			console.log(`User left Room: ${socket.currentRoom}`);
+		}
+		socket.join(room);
+		socket.currentRoom = room;
+		console.log("User joined Room:", room);
+	};
 
-    const typingHandler = (room) => {
-        socket.in(room).emit("typing");
-    };
+	const typingHandler = (room) => {
+		socket.in(room).emit("typing");
+	};
 
-    const stopTypingHandler = (room) => {
-        socket.in(room).emit("stop typing");
-    };
+	const stopTypingHandler = (room) => {
+		socket.in(room).emit("stop typing");
+	};
 
-    const newMessageHandler = (newMessageReceived) => {
-        let chat = newMessageReceived.chat;
-        chat.users.forEach((user) => {
-            if (user._id === newMessageReceived.sender._id) return;
-            console.log("Message received by:", user._id);
-            socket.in(user._id).emit("message received", newMessageReceived);
-        });
-    };
+	const newMessageHandler = (newMessageReceived) => {
+		let chat = newMessageReceived.chat;
+		chat.users.forEach((user) => {
+			if (user._id === newMessageReceived.sender._id) return;
+			console.log("Message received by:", user._id);
+			socket.in(user._id).emit("message received", newMessageReceived);
+		});
+	};
 
-    socket.on("setup", setupHandler);
-    socket.on("join chat", joinChatHandler);
-    socket.on("typing", typingHandler);
-    socket.on("stop typing", stopTypingHandler);
-    socket.on("new message", newMessageHandler);
+	socket.on("setup", setupHandler);
+	socket.on("join chat", joinChatHandler);
+	socket.on("typing", typingHandler);
+	socket.on("stop typing", stopTypingHandler);
+	socket.on("new message", newMessageHandler);
 
-    socket.on("disconnect", () => {
-        console.log("User disconnected:", socket.id);
-        socket.off("setup", setupHandler);
-        socket.off("join chat", joinChatHandler);
-        socket.off("typing", typingHandler);
-        socket.off("stop typing", stopTypingHandler);
-        socket.off("new message", newMessageHandler);
-    });
+	socket.on("disconnect", () => {
+		console.log("User disconnected:", socket.id);
+		socket.off("setup", setupHandler);
+		socket.off("join chat", joinChatHandler);
+		socket.off("typing", typingHandler);
+		socket.off("stop typing", stopTypingHandler);
+		socket.off("new message", newMessageHandler);
+	});
 });
