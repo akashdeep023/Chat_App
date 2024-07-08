9a6253f3844c66432b74779c7c8786c4db1448e8
akashdeep023 <ad3500476@gmail.com>
2024-07-09 00:20:23 +0530

    socket

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
-app.use(cors({ origin: process.env.FRONTEND_URL }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3000;
