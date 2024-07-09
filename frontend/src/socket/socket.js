bcb657028d19b4fe943bafb41d746d59fb645601
akashdeep023 <ad3500476@gmail.com>
2024-07-10 00:21:38 +0530

    socket update

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-const ENDPOINT = import.meta.env.VITE_BACKEND_URL;
+const ENDPOINT = `${import.meta.env.VITE_BACKEND_URL}`;
 const socket = io(ENDPOINT, {
 	transports: ["websocket"],
 });
