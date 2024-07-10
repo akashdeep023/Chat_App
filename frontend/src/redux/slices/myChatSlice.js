fa8a5fc48bf8e668a05c01a98597890abda5407d
akashdeep023 <ad3500476@gmail.com>
2024-07-11 01:52:14 +0530

    Add delete Selected Chat

--- frontend/src/redux/slices/myChatSlice.js
+++ frontend/src/redux/slices/myChatSlice.js
@@ -1,40 +1,48 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const myChatSlice = createSlice({
-    name: "myChat",
-    initialState: {
-        chat: [],
-        selectedChat: null,
-        newMessageRecieved: [],
-    },
-    reducers: {
-        addMyChat: (state, action) => {
-            state.chat = action.payload;
-        },
-        addSelectedChat: (state, action) => {
-            state.selectedChat = action.payload;
-        },
-        addNewMessageRecieved: (state, action) => {
-            if (!state.newMessageRecieved.includes(action.payload)) {
-                state.newMessageRecieved = [
-                    action.payload,
-                    ...state.newMessageRecieved,
-                ];
-            }
-        },
-        removeNewMessageRecieved: (state, action) => {
-            const newMessageRecieved = state.newMessageRecieved.filter(
-                (message) => message.chat._id !== action.payload.chat._id
-            );
-            state.newMessageRecieved = newMessageRecieved;
-        },
-    },
+	name: "myChat",
+	initialState: {
+		chat: [],
+		selectedChat: null,
+		newMessageRecieved: [],
+	},
+	reducers: {
+		addMyChat: (state, action) => {
+			state.chat = action.payload;
+		},
+		addSelectedChat: (state, action) => {
+			state.selectedChat = action.payload;
+		},
+		deleteSelectedChat: (state, action) => {
+			state.selectedChat = null;
+			const allChat = state.chat.filter(
+				(chat) => chat._id !== action.payload
+			);
+			state.chat = allChat;
+		},
+		addNewMessageRecieved: (state, action) => {
+			if (!state.newMessageRecieved.includes(action.payload)) {
+				state.newMessageRecieved = [
+					action.payload,
+					...state.newMessageRecieved,
+				];
+			}
+		},
+		removeNewMessageRecieved: (state, action) => {
+			const newMessageRecieved = state.newMessageRecieved.filter(
+				(message) => message.chat._id !== action.payload.chat._id
+			);
+			state.newMessageRecieved = newMessageRecieved;
+		},
+	},
 });
 
 export const {
-    addMyChat,
-    addSelectedChat,
-    addNewMessageRecieved,
-    removeNewMessageRecieved,
+	addMyChat,
+	addSelectedChat,
+	deleteSelectedChat,
+	addNewMessageRecieved,
+	removeNewMessageRecieved,
 } = myChatSlice.actions;
 export default myChatSlice.reducer;
