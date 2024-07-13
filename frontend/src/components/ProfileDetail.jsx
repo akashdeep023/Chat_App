05137e07443a8440147f47a33ca92defeb23f5bb
akashdeep023 <ad3500476@gmail.com>
2024-07-14 00:54:31 +0530

    Update

--- frontend/src/components/ProfileDetail.jsx
+++ frontend/src/components/ProfileDetail.jsx
@@ -2,66 +2,73 @@ import React from "react";
 import { MdOutlineClose } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import { setProfileDetail } from "../redux/slices/conditionSlice";
+import { toast } from "react-toastify";
 
 const ProfileDetail = () => {
-    const dispatch = useDispatch();
-    const user = useSelector((store) => store.auth);
-    return (
-        <div className="flex -m-2 sm:-m-4 flex-col items-center my-6 text-slate-300 min-h-screen w-full fixed top-0 justify-center z-50">
-            <div className="p-3 pt-4 w-[80%] sm:w-[60%] md:w-[50%] lg:w-[40%] min-w-72 max-w-[1000px] border border-slate-400 bg-slate-800 rounded-lg h-fit mt-5 transition-all relative">
-                <h2 className="text-2xl underline underline-offset-8 font-semibold text-slate-100 w-full text-center mb-2">
-                    Profile
-                </h2>
-                <div className="w-full py-4 justify-evenly flex flex-wrap items-center gap-3">
-                    <div className="self-end">
-                        <h3 className="text-xl font-semibold p-1">
-                            Name : {user.firstName} {user.lastName}
-                        </h3>
-                        <h3 className="text-xl font-semibold p-1">
-                            Email : {user.email}
-                        </h3>
-                        <button
-                            onClick={() => {
-                                localStorage.removeItem("token");
-                                window.location.reload();
-                            }}
-                            className="bg-red-500 hover:bg-red-700 text-white font-bold py-1.5 px-4 rounded mt-3 hidden sm:block"
-                        >
-                            Logout
-                        </button>
-                    </div>
-                    <div className="self-end flex w-full sm:w-fit items-center justify-evenly sm:flex-col">
-                        <img
-                            src={user.image}
-                            alt="user/image"
-                            className="w-20 h-20 rounded-md"
-                        />
-                        <div className="flex flex-col">
-                            <button className="bg-blue-800 hover:bg-blue-900 text-white font-bold py-1.5 px-4 rounded sm:mt-3">
-                                Update
-                            </button>
-                            <button
-                                onClick={() => {
-                                    localStorage.removeItem("token");
-                                    window.location.reload();
-                                }}
-                                className="bg-red-500 hover:bg-red-700 text-white font-bold py-1.5 px-4 rounded mt-3 sm:hidden"
-                            >
-                                Logout
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div
-                    title="Close"
-                    onClick={() => dispatch(setProfileDetail())}
-                    className="bg-black/15 hover:bg-black/50 h-7 w-7 rounded-md flex items-center justify-center absolute top-2 right-3 cursor-pointer"
-                >
-                    <MdOutlineClose size={22} />
-                </div>
-            </div>
-        </div>
-    );
+	const dispatch = useDispatch();
+	const user = useSelector((store) => store.auth);
+	const handleUpdate = () => {
+		toast.warn("Coming soon");
+	};
+	return (
+		<div className="flex -m-2 sm:-m-4 flex-col items-center my-6 text-slate-300 min-h-screen w-full fixed top-0 justify-center z-50">
+			<div className="p-3 pt-4 w-[80%] sm:w-[60%] md:w-[50%] lg:w-[40%] min-w-72 max-w-[1000px] border border-slate-400 bg-slate-800 rounded-lg h-fit mt-5 transition-all relative">
+				<h2 className="text-2xl underline underline-offset-8 font-semibold text-slate-100 w-full text-center mb-2">
+					Profile
+				</h2>
+				<div className="w-full py-4 justify-evenly flex flex-wrap items-center gap-3">
+					<div className="self-end">
+						<h3 className="text-xl font-semibold p-1">
+							Name : {user.firstName} {user.lastName}
+						</h3>
+						<h3 className="text-xl font-semibold p-1">
+							Email : {user.email}
+						</h3>
+						<button
+							onClick={() => {
+								localStorage.removeItem("token");
+								window.location.reload();
+							}}
+							className="bg-red-500 hover:bg-red-700 text-white font-bold py-1.5 px-4 rounded mt-3 hidden sm:block"
+						>
+							Logout
+						</button>
+					</div>
+					<div className="self-end flex w-full sm:w-fit items-center justify-evenly sm:flex-col">
+						<img
+							src={user.image}
+							alt="user/image"
+							className="w-20 h-20 rounded-md"
+						/>
+						<div className="flex flex-col">
+							<button
+								onClick={handleUpdate}
+								className="bg-blue-800 hover:bg-blue-900 text-white font-bold py-1.5 px-4 rounded sm:mt-3"
+							>
+								Update
+							</button>
+							<button
+								onClick={() => {
+									localStorage.removeItem("token");
+									window.location.reload();
+								}}
+								className="bg-red-500 hover:bg-red-700 text-white font-bold py-1.5 px-4 rounded mt-3 sm:hidden"
+							>
+								Logout
+							</button>
+						</div>
+					</div>
+				</div>
+				<div
+					title="Close"
+					onClick={() => dispatch(setProfileDetail())}
+					className="bg-black/15 hover:bg-black/50 h-7 w-7 rounded-md flex items-center justify-center absolute top-2 right-3 cursor-pointer"
+				>
+					<MdOutlineClose size={22} />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ProfileDetail;
--- frontend/src/components/messageComponents/MessageSend.jsx
+++ frontend/src/components/messageComponents/MessageSend.jsx
@@ -4,8 +4,8 @@ import { MdOutlineClose } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import { setSendLoading, setTyping } from "../../redux/slices/conditionSlice";
 import {
-    addNewMessage,
-    addNewMessageId,
+	addNewMessage,
+	addNewMessageId,
 } from "../../redux/slices/messageSlice";
 import { LuLoader } from "react-icons/lu";
 import { toast } from "react-toastify";
@@ -13,98 +13,100 @@ import socket from "../../socket/socket";
 
 let lastTypingTime;
 const MessageSend = ({ chatId }) => {
-    const mediaFile = useRef();
-    const [newMessage, setMessage] = useState("");
-    const [mediaBox, setMediaBox] = useState(false);
-    const [mediaURL, setMediaURL] = useState("");
-    const dispatch = useDispatch();
-    const isSendLoading = useSelector(
-        (store) => store?.condition?.isSendLoading
-    );
-    const isSocketConnected = useSelector(
-        (store) => store?.condition?.isSocketConnected
-    );
-    const selectedChat = useSelector((store) => store?.myChat?.selectedChat);
-    const isTyping = useSelector((store) => store?.condition?.isTyping);
+	const mediaFile = useRef();
+	// const [mediaBox, setMediaBox] = useState(false);
+	// const [mediaURL, setMediaURL] = useState("");
+	const [newMessage, setMessage] = useState("");
+	const dispatch = useDispatch();
+	const isSendLoading = useSelector(
+		(store) => store?.condition?.isSendLoading
+	);
+	const isSocketConnected = useSelector(
+		(store) => store?.condition?.isSocketConnected
+	);
+	const selectedChat = useSelector((store) => store?.myChat?.selectedChat);
+	const isTyping = useSelector((store) => store?.condition?.isTyping);
 
-    useEffect(() => {
-        socket.on("typing", () => dispatch(setTyping(true)));
-        socket.on("stop typing", () => dispatch(setTyping(false)));
-    }, []);
+	useEffect(() => {
+		socket.on("typing", () => dispatch(setTyping(true)));
+		socket.on("stop typing", () => dispatch(setTyping(false)));
+	}, []);
 
-    // Media Box Control
-    const handleMediaBox = () => {
-        if (mediaFile.current?.files[0]) {
-            const file = mediaFile.current.files[0];
-            const url = URL.createObjectURL(file);
-            setMediaURL(url);
-            setMediaBox(true);
-            toast.warn("Media Not Sending");
-        } else {
-            setMediaBox(false);
-        }
-    };
-    // Media Box Hidden && Input file remove
-    const clearMediaFile = () => {
-        mediaFile.current.value = "";
-        setMediaURL("");
-        setMediaBox(false);
-    };
-    // Send Message Api call
-    const handleSendMessage = async () => {
-        if (newMessage?.trim()) {
-            const message = newMessage?.trim();
-            setMessage("");
-            socket.emit("stop typing", selectedChat._id);
-            dispatch(setSendLoading(true));
-            const token = localStorage.getItem("token");
-            fetch(`${import.meta.env.VITE_BACKEND_URL}/api/message`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`,
-                },
-                body: JSON.stringify({
-                    message: message,
-                    chatId: chatId,
-                }),
-            })
-                .then((res) => res.json())
-                .then((json) => {
-                    dispatch(addNewMessageId(json?.data?._id));
-                    dispatch(addNewMessage(json?.data));
-                    socket.emit("new message", json.data);
-                    dispatch(setSendLoading(false));
-                })
-                .catch((err) => {
-                    console.log(err);
-                    dispatch(setSendLoading(false));
-                    toast.error("Message Sending Failed");
-                });
-        }
-    };
+	// Media Box Control
+	const handleMediaBox = () => {
+		if (mediaFile.current?.files[0]) {
+			// const file = mediaFile.current.files[0];
+			// const url = URL.createObjectURL(file);
+			// setMediaURL(url);
+			// setMediaBox(true);
+			toast.warn("Comming soon...");
+		} else {
+			// setMediaBox(false);
+		}
+	};
 
-    const handleTyping = (e) => {
-        setMessage(e.target?.value);
-        if (!isSocketConnected) return;
-        if (!isTyping) {
-            socket.emit("typing", selectedChat._id);
-        }
-        lastTypingTime = new Date().getTime();
-        let timerLength = 3000;
-        let stopTyping = setTimeout(() => {
-            let timeNow = new Date().getTime();
-            let timeDiff = timeNow - lastTypingTime;
-            if (timeDiff > timerLength) {
-                socket.emit("stop typing", selectedChat._id);
-            }
-        }, timerLength);
-        return () => clearTimeout(stopTyping);
-    };
+	// Media Box Hidden && Input file remove
+	// const clearMediaFile = () => {
+	//     mediaFile.current.value = "";
+	//     setMediaURL("");
+	//     setMediaBox(false);
+	// };
 
-    return (
-        <>
-            {mediaBox && (
+	// Send Message Api call
+	const handleSendMessage = async () => {
+		if (newMessage?.trim()) {
+			const message = newMessage?.trim();
+			setMessage("");
+			socket.emit("stop typing", selectedChat._id);
+			dispatch(setSendLoading(true));
+			const token = localStorage.getItem("token");
+			fetch(`${import.meta.env.VITE_BACKEND_URL}/api/message`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+				body: JSON.stringify({
+					message: message,
+					chatId: chatId,
+				}),
+			})
+				.then((res) => res.json())
+				.then((json) => {
+					dispatch(addNewMessageId(json?.data?._id));
+					dispatch(addNewMessage(json?.data));
+					socket.emit("new message", json.data);
+					dispatch(setSendLoading(false));
+				})
+				.catch((err) => {
+					console.log(err);
+					dispatch(setSendLoading(false));
+					toast.error("Message Sending Failed");
+				});
+		}
+	};
+
+	const handleTyping = (e) => {
+		setMessage(e.target?.value);
+		if (!isSocketConnected) return;
+		if (!isTyping) {
+			socket.emit("typing", selectedChat._id);
+		}
+		lastTypingTime = new Date().getTime();
+		let timerLength = 3000;
+		let stopTyping = setTimeout(() => {
+			let timeNow = new Date().getTime();
+			let timeDiff = timeNow - lastTypingTime;
+			if (timeDiff > timerLength) {
+				socket.emit("stop typing", selectedChat._id);
+			}
+		}, timerLength);
+		return () => clearTimeout(stopTyping);
+	};
+
+	return (
+		<>
+			{/* {mediaBox && (
                 <div className="border-slate-500 border rounded-md absolute bottom-[7vh] mb-1 left-2 bg-slate-800 w-60 h-48 ">
                     <img
                         src={mediaURL}
@@ -118,60 +120,60 @@ const MessageSend = ({ chatId }) => {
                         onClick={clearMediaFile}
                     />
                 </div>
-            )}
-            <form
-                className="w-full flex items-center gap-1 h-[7vh] p-3 bg-slate-800 text-white"
-                onSubmit={(e) => e.preventDefault()}
-            >
-                <label htmlFor="media" className="cursor-pointer">
-                    <FaFolderOpen
-                        title="Open File"
-                        size={22}
-                        className="active:scale-75 hover:text-green-400"
-                    />
-                </label>
-                <input
-                    ref={mediaFile}
-                    type="file"
-                    name="image"
-                    accept="image/png, image/jpg, image/gif, image/jpeg"
-                    id="media"
-                    className="hidden"
-                    onChange={handleMediaBox}
-                />
-                <input
-                    type="text"
-                    className="outline-none p-2 w-full bg-transparent"
-                    placeholder="Type a message"
-                    value={newMessage}
-                    onChange={(e) => handleTyping(e)}
-                />
-                <span className="flex justify-center items-center">
-                    {newMessage?.trim() && !isSendLoading && (
-                        <button
-                            className="outline-none p-2 border-slate-500 border-l"
-                            onClick={handleSendMessage}
-                        >
-                            <FaPaperPlane
-                                title="Send"
-                                size={18}
-                                className="active:scale-75 hover:text-green-400"
-                            />
-                        </button>
-                    )}
-                    {isSendLoading && (
-                        <button className="outline-none p-2 border-slate-500 border-l">
-                            <LuLoader
-                                title="loading..."
-                                fontSize={18}
-                                className="animate-spin"
-                            />
-                        </button>
-                    )}
-                </span>
-            </form>
-        </>
-    );
+            )} */}
+			<form
+				className="w-full flex items-center gap-1 h-[7vh] p-3 bg-slate-800 text-white"
+				onSubmit={(e) => e.preventDefault()}
+			>
+				<label htmlFor="media" className="cursor-pointer">
+					<FaFolderOpen
+						title="Open File"
+						size={22}
+						className="active:scale-75 hover:text-green-400"
+					/>
+				</label>
+				<input
+					ref={mediaFile}
+					type="file"
+					name="image"
+					accept="image/png, image/jpg, image/gif, image/jpeg"
+					id="media"
+					className="hidden"
+					onChange={handleMediaBox}
+				/>
+				<input
+					type="text"
+					className="outline-none p-2 w-full bg-transparent"
+					placeholder="Type a message"
+					value={newMessage}
+					onChange={(e) => handleTyping(e)}
+				/>
+				<span className="flex justify-center items-center">
+					{newMessage?.trim() && !isSendLoading && (
+						<button
+							className="outline-none p-2 border-slate-500 border-l"
+							onClick={handleSendMessage}
+						>
+							<FaPaperPlane
+								title="Send"
+								size={18}
+								className="active:scale-75 hover:text-green-400"
+							/>
+						</button>
+					)}
+					{isSendLoading && (
+						<button className="outline-none p-2 border-slate-500 border-l">
+							<LuLoader
+								title="loading..."
+								fontSize={18}
+								className="animate-spin"
+							/>
+						</button>
+					)}
+				</span>
+			</form>
+		</>
+	);
 };
 
 export default MessageSend;
